d13e78665d2bba56bc5566a807ec85a10d3df9f1
Sophia Zhang <ssz.windyskies@gmail.com>
2023-08-31 10:03:25 -0700

    set up + openAccount

---
+++ ATM.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+
+public class ATM
+{
+    private HashMap <String, Double> atmMachine;
+
+    public ATM ()
+    {
+        atmMachine = new HashMap <String, Double> ();
+    }
+    public void openAccount (String userID, double amount)
+    {
+        //if user already exists, throw error
+        if (atmMachine.containsKey (userID))
+        {
+            System.out.println ("Error: User already exists.");
+        }
+        else
+        {
+            atmMachine.put (userID, amount);
+        }
+    }
+    public void closeAccount (String userID)
+    {
+
+    }
+    public double checkBalance (String userID)
+    {
+
+    }
+    public double depositMoney (String userID, double amount)
+    {
+
+    }
+    public double withdrawMoney (String userID, double amount)
+    {
+
+    }
+    public boolean transferMoney (String fromAccount, String toAccount,double amount)
+    {
+
+    }
+    public void audit ()
+    {
+
+    }
+}
---
+++ ATMTester.java
@@ -0,0 +1,9 @@
+public class ATMTester
+{
+    public static void main (String [] args)
+    {
+        ATM bank = new ATM ();
+        bank.openAccount("newDude", 100);
+        bank.openAccount ("newDude", 10); //should throw error
+    }
+}
